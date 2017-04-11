e5790ef4a4cb96e06c8e37988ad02868d8f320e3
tdrier1 <tdrier1@gmail.com>
2017-04-11 15:31:00 -0400

    created table

---
+++ Lab_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learn you squares and cubes!");
+
+            Console.WriteLine("Enter an integer: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Number \t Squared \t Cubed");
+            Console.WriteLine("======\t ====== \t =====");
+
+
+        }
+    }
+}
